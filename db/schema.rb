fc68b3d4780b24f03b5af9d2f17d028e535a05b7
Dante DiClemente <diclementedante@gmail.com>
2016-07-20 12:57:10 -0400

    remove pro pic

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20160719233350) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "profile_photo"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
