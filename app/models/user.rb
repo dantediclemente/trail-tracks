2fc143f004b9a8cdf087da953fdd5c2d0d091c13
Dante DiClemente <diclementedante@gmail.com>
2016-07-20 13:16:20 -0400

    Revert "a"
    This reverts commit 929ca0869ef100ae0c9cd60497ff52329543ce8e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessor :password, :password_confirmation, :current_password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
